222cee9af38fc23570570fc115bce9366cf57357
osigie <90554061+osigie@users.noreply.github.com>
2022-06-17 14:24:23 +0100

    removed image

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,12 +1,10 @@
 import Wrapper from "../../assets/wrappers/ErrorPage";
 import { Link } from "react-router-dom";
-import img from "../assets/images/not-found.svg";
 
 const NotFound = () => {
   return (
     <Wrapper className="full-page">
       <div>
-        <img src={img} alt="not found" />
         <h3> Ohh! page not found </h3>
         <p>We can't seem to find the page you are looking for</p>
         <Link to="/"> Back Home</Link>
